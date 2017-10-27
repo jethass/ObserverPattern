633827816d3c9d59c0be05e41e4af8c32351b84b
jethass <jethass@hotmail.com>
2017-10-27 11:51:23 +0200

    Update Observer.php

--- Observer.php
+++ Observer.php
@@ -3,7 +3,7 @@ class Observer implements SplObserver
 {
   public function update(SplSubject $obj)
   {
-    echo 'Observer1 a été notifié ! Nouvelle valeur de l\'attribut <strong>nom</strong> : ', $obj->getNom();
+    echo 'Observer a été notifié ! Nouvelle valeur de l\'attribut <strong>nom</strong> : ', $obj->getNom();
   }
 }
 
